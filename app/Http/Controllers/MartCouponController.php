c2eed81137267000670b753ed7c4f8551ab6162e
焦品貴 <jpink0210@gmail.com>
2023-09-08 10:44:21 +0800

    project init

---
+++ app/Http/Controllers/MartCouponController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MartCoupon;
+use Illuminate\Http\Request;
+
+class MartCouponController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MartCoupon $martCoupon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MartCoupon $martCoupon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MartCoupon $martCoupon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MartCoupon $martCoupon)
+    {
+        //
+    }
+}
