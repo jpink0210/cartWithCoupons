dbbd75a38f73ee63ad00a278ea369c6d67a685ce
焦品貴 <jpink0210@gmail.com>
2023-09-08 21:28:25 +0800

    [Account] -mr

---
+++ database/migrations/2023_09_08_125129_create_accounts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('account')->comment("user帳號");
+            $table->integer('deposit')->nullable()->default(0)->comment("存款餘額");
+            $table->text('infomation')->nullable()->default(0)->comment("詳細資料");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
