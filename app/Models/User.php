744d445cdcfa66cd5a1ba4f8d973ebba0e36b977
焦品貴 <jpink0210@gmail.com>
2023-09-08 11:44:24 +0800

    [PassPort] 補齊設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+/**
+ * composer require laravel/passport
+ * php artisan migrate
+ * php artisan passport:install
+ * 
+ */
 
 class User extends Authenticatable
 {
